948a3bca16d5402e4601b5e2a0cdd8233acdd24a
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-11-04 22:32:12 +0300

    fix static

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
+func index(w http.ResponseWriter, req *http.Request) {
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		fmt.Fprintf(w, "%s", err)
+	}
+	t.ExecuteTemplate(w, "index.html", nil)
+}
+
+func handleFunc() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8082", nil)
+}
+
+func main() {
+	fmt.Println("GGG")
 }
